2785cd5a46772d8bc44a0cd58a8864ba44cc8341
ieunjung <euju718@gmail.com>
2020-03-20 11:41:50 +0900

    리덕스 코드 개선

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ const add = document.getElementById("add")
 const minus = document.getElementById("minus")
 const number = document.querySelector("span");
 
+const ADD = "ADD";
+const MINUS = "MINUS";
 
 // * reducer : a function that modifies the data(state)
 const countModifier = (count = 0, action) => {
-    if(action.type === "ADD"){
-        return count + 1;
-    }else if(action.type === "MINUS"){
-        return count - 1;
-    }else {
-        return count;
+    switch (action.type) {
+        case ADD:
+            return count + 1;
+        case MINUS:
+            return count - 1;
+        default:
+            return count;
     }
 };
 
@@ -27,14 +30,13 @@ const onChange = () => {
 // subscribe : to allow us listen for changes in our stroe.
 countStore.subscribe(onChange);
 
-
-// a way of communicating with a reducer is to dispatch, sending a action!
+// action : a way of communicating with a reducer is to dispatch, sending a action!
 const handleAdd = () => {
-    countStore.dispatch({type: "ADD"});
+    countStore.dispatch({type: ADD });
 }
 
 const handleMinus = () => {
-    countStore.dispatch({type: "MINUS"});
+    countStore.dispatch({type: MINUS });
 }
 
 add.addEventListener("click", handleAdd);
